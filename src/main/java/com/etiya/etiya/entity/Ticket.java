0f9f019bc26fc75e35e2e0f028cecd4f8626d44d
MustafaCemil <mstf75cml@gmail.com>
2019-08-22 15:15:28 +0300

    Latest version

--- src/main/java/com/etiya/etiya/entity/Ticket.java
+++ src/main/java/com/etiya/etiya/entity/Ticket.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 
 @Entity
-@Table(name = "flights")
+@Table(name = "tickets")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
